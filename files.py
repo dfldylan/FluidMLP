981cbb53f54ec8d5239aff142ed25cdbab473999
dufeilong <dufeilong@AFISH-WS-DFL>
2020-12-10 21:36:07 +0800

    seperate files.py

--- files.py
+++ files.py
@@ -2,20 +2,22 @@ import pandas as pd
 import numpy as np
 import os
 import random
-import config as cfg
 
 
-def build_data(pos, vel, s_f, out):
-    voxel = np.floor(pos * (1 / cfg.voxel_size)).astype(int)
+def build_data(pos, vel, s_f, out, voxel_size):
+    if voxel_size:
+        voxel = np.floor(pos * (1 / voxel_size)).astype(int)
+    else:
+        voxel = np.zeros_like(pos)
     data = np.concatenate([pos, vel, s_f, out, voxel], axis=1)
     return data
 
 
-def get_data_from_file(file_path):
+def get_data_from_file(file_path, voxel_size=None):
     df = pd.read_csv(file_path, dtype=float)
     df['isFluidSolid'] = df['isFluidSolid'].astype(int)
     return build_data(df.iloc[:, :3].values, df.iloc[:, 3:6].values, df.iloc[:, 7:8].values,
-                      df.iloc[:, 12:15].values - df.iloc[:, 3:6].values)
+                      df.iloc[:, 12:15].values - df.iloc[:, 3:6].values, voxel_size)
 
 
 def find_files(root_path):
